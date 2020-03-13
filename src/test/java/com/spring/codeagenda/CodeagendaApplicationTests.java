f2e510310a68b1773dc5e7a526590cb3df5305c5
Thiago Lino <thiagolinoz@gmail.com>
2020-03-12 23:44:10 -0300

    first commit

---
+++ src/test/java/com/spring/codeagenda/CodeagendaApplicationTests.java
@@ -0,0 +1,13 @@
+package com.spring.codeagenda;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CodeagendaApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
