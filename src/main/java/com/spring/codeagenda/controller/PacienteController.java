a03d2b5fcd5ce3d7b50dbf33fc039ef2e695e968
Thiago Lino <thiagolinoz@gmail.com>
2020-08-20 08:35:15 -0300

    implemented helper to format LocalDate

--- src/main/java/com/spring/codeagenda/controller/PacienteController.java
+++ src/main/java/com/spring/codeagenda/controller/PacienteController.java
@@ -16,90 +16,99 @@ import org.springframework.web.servlet.mvc.support.RedirectAttributes;
 import com.spring.codeagenda.model.Paciente;
 import com.spring.codeagenda.service.CreateCodeService;
 import com.spring.codeagenda.service.PacienteService;
+import com.spring.codeagenda.utils.DateBRFormattHelper;
 
 import javassist.NotFoundException;
 
 @Controller
 public class PacienteController {
-	
+
 	@Autowired
 	PacienteService pacienteService;
 	@Autowired
 	CreateCodeService createNewCode;
-	
+	@Autowired
+	DateBRFormattHelper dateBRFormatter;
+
 	@RequestMapping(value = "/pacientes", method = RequestMethod.GET)
 	public ModelAndView getPacientes() {
-		
+
 		ModelAndView mv = new ModelAndView("Paciente/pacientes");
 		List<Paciente> pacientes = pacienteService.findAll();
 		mv.addObject("pacientes", pacientes);
-		
+
 		return mv;
 	}
-	
+
 	@RequestMapping(value = "/pacientes/{id}", method = RequestMethod.GET)
 	public ModelAndView getPacienteDetails(@PathVariable("id") long id) throws NotFoundException {
-		
+
 		ModelAndView mv = new ModelAndView("Paciente/pacienteDetails");
-		
+
 		try {
 			Paciente paciente = pacienteService.findById(id);
+			
+			String dataInicioFormatted = dateBRFormatter.conversion(paciente.getDataInicio());
+			String dataNascimentoFormatted = dateBRFormatter.conversion(paciente.getDataNascimento());
+			
 			mv.addObject("paciente", paciente);
+			mv.addObject("dataInicioFormatted", dataInicioFormatted);
+			mv.addObject("dataNascimentoFormatted", dataNascimentoFormatted);
 		} catch (Exception exception) {
 			mv.setViewName("notFound");
-		} 
-		
+		}
+
 		return mv;
 	}
-	
+
 	@RequestMapping(value = "/newpaciente", method = RequestMethod.GET)
 	public String getPacienteForm() {
 		return "Paciente/pacienteForm";
 	}
-	
+
 	@RequestMapping(value = "/editpaciente/{userId}", method = RequestMethod.GET)
 	public ModelAndView showPacienteForm(@PathVariable("userId") long userId) throws NotFoundException {
-		
+
 		ModelAndView mv = new ModelAndView("Paciente/editPacienteForm");
-		
+
 		try {
 			Paciente paciente = pacienteService.findById(userId);
 			mv.addObject("paciente", paciente);
 		} catch (Exception exception) {
 			mv.setViewName("notFound");
-		} 
-		
+		}
+
 		return mv;
 	}
-	
+
 	@RequestMapping(value = "/save", method = RequestMethod.POST)
 	public String savePaciente(@Valid Paciente paciente, BindingResult result, RedirectAttributes attributes) {
-		
-		if(result.hasErrors()) {
+
+		if (result.hasErrors()) {
 			return "redirect:/pacientes";
 		}
-		
-		if(paciente.getId() == null) {
+
+		if (paciente.getId() == null) {
 			String novoCodigo = createNewCode.novoCodigo();
 			paciente.setCodigoPaciente(novoCodigo);
 		}
 
 		pacienteService.save(paciente);
-		
+
 		return "redirect:/pacientes";
 	}
-	
+
 	@RequestMapping(value = "/delete/{userId}", method = RequestMethod.GET)
 	public String deletePaciente(@PathVariable("userId") long userId) throws NotFoundException {
-		
+
 		try {
 			Paciente paciente = pacienteService.findById(userId);
 			pacienteService.delete(paciente);
 		} catch (Exception e) {
 			return "redirect:/notFound";
 		}
-		
+
 		return "redirect:/pacientes";
 	}
-	
+
 }
